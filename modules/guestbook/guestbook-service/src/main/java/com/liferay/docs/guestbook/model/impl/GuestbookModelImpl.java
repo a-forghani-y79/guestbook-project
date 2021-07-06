f5fdd262c0e4989341bcec31e2cb3b15481eaa31
ali forghani <a.forghani.y79@gmail.com>
2021-07-06 14:58:54 +0430

    enabling assets at the service layer

--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/model/impl/GuestbookModelImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/model/impl/GuestbookModelImpl.java
@@ -137,14 +137,20 @@ public class GuestbookModelImpl
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long UUID_COLUMN_BITMASK = 4L;
+	public static final long STATUS_COLUMN_BITMASK = 4L;
+
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
+	 */
+	@Deprecated
+	public static final long UUID_COLUMN_BITMASK = 8L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
 	 *		#getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long GUESTBOOKID_COLUMN_BITMASK = 8L;
+	public static final long GUESTBOOKID_COLUMN_BITMASK = 16L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
@@ -625,6 +631,16 @@ public class GuestbookModelImpl
 		}
 
 		_status = status;
+	}
+
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 *             #getColumnOriginalValue(String)}
+	 */
+	@Deprecated
+	public int getOriginalStatus() {
+		return GetterUtil.getInteger(
+			this.<Integer>getColumnOriginalValue("status"));
 	}
 
 	@JSON
